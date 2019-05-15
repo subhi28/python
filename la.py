42c6df0271dde0913549d367e5b41eeddfb17b27
subhi28 <50619862+subhi28@users.noreply.github.com>
2019-05-15 10:14:54 +0530

    Update la.py

--- la.py
+++ la.py
@@ -1,7 +1,10 @@
 num1,num2,num3=input().split()
-if(num1>num2) and (num1>num3):
-    print(num1)
-elif(num2>num1) and (num2>num3):
-    print(num2)
+n1=int(num1)
+n2=int(num2)
+n3=int(num3)
+if(n1>n2) and (n1>n3):
+    print(n1)
+elif(n2>n1) and (n2>n3):
+    print(n2)
 else:
-    print(num3)
+    print(n3)
