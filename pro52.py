c717703bac740c5defc5e6c13f96ffc8b1b89317
subhi28 <50619862+subhi28@users.noreply.github.com>
2019-07-25 18:48:19 +0530

    Create pro52.py

---
+++ pro52.py
@@ -0,0 +1,9 @@
+#subi
+p=input().split()
+q=input().split()
+r=input().split()
+s=input().split()
+if(p[0]==q[0] or p[0]==r[0] or p[0]==s[0]) and (p[1]==q[1] or p[1]==r[1] or p[1]==s[1]):
+  print('yes')
+else:
+  print('no')
