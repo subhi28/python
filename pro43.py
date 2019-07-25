62a5e4a7e28f27a00519582fbf973e7de96cf8b6
subhi28 <50619862+subhi28@users.noreply.github.com>
2019-07-25 19:55:08 +0530

    Create pro43.py

---
+++ pro43.py
@@ -0,0 +1,22 @@
+#subi
+v6=int(input())
+k=list(map(int,input().split()))
+s,f=0,[]
+d=[x for x in range(1,v6+1)]
+for i in k:
+  if i in d:
+    d.remove(i)
+m=0
+for i in range(0,v6-1):
+  h=k[i]
+  for j in range(i+1,v6):
+    if h==k[j]:
+      if h<d[m]:
+        k[j]=d[m]
+        s+=1
+      else:
+        k[i]=d[m]
+        s+=1
+      m+=1
+print(s)
+print(*k)
