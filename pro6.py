5cf0c11fb487b09561b86312df14e95f21365b79
subhi28 <50619862+subhi28@users.noreply.github.com>
2019-07-25 20:21:31 +0530

    Create pro6.py

---
+++ pro6.py
@@ -0,0 +1,10 @@
+#subi
+p = int(input())
+q = list(map(int,input().split()))
+c = 0
+for i in range(p):
+    for j in range(i,p):  
+        for k in range(j,p):
+            if q[i]<q[j]<q[k]:
+                c+=1
+print(c)
